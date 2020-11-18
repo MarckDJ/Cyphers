f3f0d1303b8d7f6b7f79b8a1f27ebcca0449e1ee
MarckDJ <marckdj11@gmail.com>
2020-11-17 20:02:36 -0600

    final_version

--- src/cripto.js
+++ src/cripto.js
@@ -1,15 +1,24 @@
 const CryptoJS = require("crypto-js");
+const base65ArrayBuffer = require("./base64ArrayBuffer");
 
 var DES =
     DES ||
     (() => {
         var cripto = (msg, key, command) => {
             if (command == true) {
-                var secret = CryptoJS.DES.encrypt(msg, key);
-                return secret;
+                var msgWordArrray = CryptoJS.lib.WordArray.create(msg); // Convert: ArrayBuffer -> WordArray
+                var encrypted = CryptoJS.DES.encrypt(
+                    msgWordArrray,
+                    key
+                ).toString();
+                console.log(encrypted); // Encryption: I: WordArray -> O: -> Base64 encoded string (OpenSSL-format)
+                return encrypted;
             } else {
                 var secret = CryptoJS.DES.decrypt(msg, key);
-                return secret.toString(CryptoJS.enc.Utf8);
+                console.log(secret);
+                var typedArray = CryptJsWordArrayToUint8Array(secret);
+                var clear = uintToString(typedArray);
+                return clear;
             }
         };
         return {
@@ -27,11 +36,18 @@ var AES =
     (() => {
         var cripto = (msg, key, command) => {
             if (command == true) {
-                var secret = CryptoJS.AES.encrypt(msg, key);
-                return secret;
+                var msgWordArrray = CryptoJS.lib.WordArray.create(msg);
+                var encrypted = CryptoJS.AES.encrypt(
+                    msgWordArrray,
+                    key
+                ).toString();
+                return encrypted;
             } else {
                 var secret = CryptoJS.AES.decrypt(msg, key);
-                return secret.toString(CryptoJS.enc.Utf8);
+                console.log(secret);
+                var typedArray = CryptJsWordArrayToUint8Array(secret);
+                var clear = uintToString(typedArray);
+                return clear;
             }
         };
         return {
@@ -48,3 +64,31 @@ module.exports = {
     DES: DES,
     AES: AES,
 };
+
+function CryptJsWordArrayToUint8Array(wordArray) {
+    const l = wordArray.sigBytes;
+    const words = wordArray.words;
+    const result = new Uint8Array(l);
+    var i = 0 /*dst*/,
+        j = 0; /*src*/
+    while (true) {
+        // here i is a multiple of 4
+        if (i == l) break;
+        var w = words[j++];
+        result[i++] = (w & 0xff000000) >>> 24;
+        if (i == l) break;
+        result[i++] = (w & 0x00ff0000) >>> 16;
+        if (i == l) break;
+        result[i++] = (w & 0x0000ff00) >>> 8;
+        if (i == l) break;
+        result[i++] = w & 0x000000ff;
+    }
+    return result;
+}
+function uintToString(uintArray) {
+    let TYPED_ARRAY = new Uint8Array(uintArray);
+    const STRING_CHAR = TYPED_ARRAY.reduce((data, byte) => {
+        return data + String.fromCharCode(byte);
+    }, "");
+    return STRING_CHAR;
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,20 +1,77 @@
 const express = require("express");
-const { AES } = require("../cripto");
 const cripto = require("../cripto");
 const router = express.Router();
+const multer = require("multer");
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "uploads/");
+    },
 
-router.post("/cipher", (req, res) => {
+    // By default, multer removes file extensions so let's add them back
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + "-" + Date.now() + ".txt");
+    },
+});
+const upload = multer({ dest: "uploads/", storage: storage });
+
+const fs = require("fs");
+
+router.post("/cipher", upload.single("document"), (req, res) => {
+    if (typeof req.file == "undefined" || req.body.key == ("" || undefined)) {
+        res.render("results.html", {
+            wrn: "No deje campos vacios",
+        });
+        return;
+    } else if (!req.file.filename.endsWith(".txt" || ".TXT")) {
+        res.render("results.html", {
+            wrn: "Actualmente solo se aceptan archivos de tipo 'txt'",
+        });
+        return;
+    }
     if (req.body.cipher == "DES") {
-        if (req.body.command == "enc") {
-            var out = cripto.DES.encriptar(req.body.msg, req.body.key);
+        if (String(req.body.key).length < 8) {
             res.render("results.html", {
-                msg: out,
+                wrn:
+                    "El tamaño de la clave es muy pequeño, use solo 8 caracteres",
+            });
+            return;
+        }
+        if (String(req.body.key).length > 8) {
+            res.render("results.html", {
+                wrn:
+                    "El tamaño de la clave es muy grande, use solo 8 caracteres",
+            });
+            return;
+        }
+        if (req.body.command == "enc") {
+            let data = fs.readFile(req.file.path, function read(err, data) {
+                if (err) {
+                    throw err;
+                }
+                var out = cripto.DES.encriptar(data.buffer, req.body.key);
+                res.render("results.html", {
+                    msg: out,
+                    name: req.file.originalname,
+                    op: "_enc",
+                });
             });
             return;
         } else if (req.body.command == "dec") {
-            var out = cripto.DES.desencriptar(req.body.msg, req.body.key);
-            res.render("results.html", {
-                msg: out,
+            let data = fs.readFile(req.file.path, "utf8", function read(
+                err,
+                data
+            ) {
+                if (err) {
+                    throw err;
+                }
+                const content = data;
+
+                var out = cripto.DES.desencriptar(content, req.body.key);
+                res.render("results.html", {
+                    msg: out,
+                    name: req.file.originalname,
+                    op: "_dec",
+                });
             });
             return;
         } else {
@@ -23,22 +80,51 @@ router.post("/cipher", (req, res) => {
             return;
         }
     } else if (req.body.cipher == "AES") {
-        if (req.body.command == "enc") {
-            var out = cripto.AES.encriptar(req.body.msg, req.body.key);
-            console.log(out);
-            res.render("results.html", {
-                msg: out,
-            });
-            return;
-        } else if (req.body.command == "dec") {
-            var out = cripto.AES.desencriptar(req.body.msg, req.body.key);
-            res.render("results.html", {
-                msg: out,
-            });
-            return;
+        if (
+            String(req.body.key).length == 16 ||
+            String(req.body.key).length == 24 ||
+            String(req.body.key).length == 32
+        ) {
+            if (req.body.command == "enc") {
+                let data = fs.readFile(req.file.path, function read(err, data) {
+                    if (err) {
+                        throw err;
+                    }
+                    var out = cripto.AES.encriptar(data.buffer, req.body.key);
+                    res.render("results.html", {
+                        msg: out,
+                        name: req.file.originalname,
+                        op: "_enc",
+                    });
+                });
+                return;
+            } else if (req.body.command == "dec") {
+                let data = fs.readFile(req.file.path, "utf8", function read(
+                    err,
+                    data
+                ) {
+                    if (err) {
+                        throw err;
+                    }
+                    const content = data;
+                    var out = cripto.AES.desencriptar(content, req.body.key);
+                    res.render("results.html", {
+                        msg: out,
+                        name: req.file.originalname,
+                        op: "_dec",
+                    });
+                });
+                return;
+            } else {
+                res.redirect("/");
+                console.log(req.body.command);
+                return;
+            }
         } else {
-            res.redirect("/");
-            console.log(req.body.command);
+            res.render("results.html", {
+                wrn:
+                    "El tamaño de la clave no es valido, use solo 16, 24, o 32 caracteres",
+            });
             return;
         }
     } else {
